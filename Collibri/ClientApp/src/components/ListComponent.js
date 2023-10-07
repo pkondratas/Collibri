71c0776f78c302c8772db10596608f490c929a4a
dovix58 <119929439+dovix58@users.noreply.github.com>
2023-10-07 22:19:32 +0300

    pries pradedant, daryt lista issisaugom :)

--- Collibri/ClientApp/src/components/ListComponent.js
+++ Collibri/ClientApp/src/components/ListComponent.js
@@ -1,6 +1,16 @@
 
 import React, { useState, useEffect } from 'react';
 import {useParams} from "react-router-dom";
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import Divider from '@mui/material/Divider';
+// import InboxIcon from '@mui/icons-material/Inbox';
+// import DraftsIcon from '@mui/icons-material/Drafts';
 
 const ListComponent = () => {
     const [sections, setSections] = useState([]);
@@ -35,6 +45,8 @@ const ListComponent = () => {
     }
     return (page
     );
+    
+    
 };
 
 export default ListComponent;
