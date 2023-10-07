443637de88e1f48820a64075230bdde67564829e
pkondratas <paulius.kondratas@gmail.com>
2023-10-08 01:05:26 +0300

    Revert "Merge branch 'master' into delete-post-by-id"
    This reverts commit e2f8d397bcc837a2a8ecf91a0fd858042518574a, reversing
    changes made to 400d6caf29cba340b1116d7f99f52f2e4453abec.

--- Collibri.Tests/Models/Posts/PostRepositoryTestData.cs
+++ Collibri.Tests/Models/Posts/PostRepositoryTestData.cs
@@ -6,10 +6,6 @@ namespace Collibri.Tests.Models.Posts
     {
         public CreatePostTestData()
         {
-<<<<<<< HEAD
-=======
-            //Correct input
->>>>>>> master
             Add(new Post(Guid.NewGuid(), "user1", "title1", 1, 0, 0, 1,  DateTime.Now, DateTime.Now));
         }
     }
@@ -18,10 +14,6 @@ namespace Collibri.Tests.Models.Posts
     {
         public GetAllPostsTestData()
         {
-<<<<<<< HEAD
-=======
-            //Correct inputs
->>>>>>> master
             Add(1, 
                 new List<Post>
                 {
@@ -33,7 +25,6 @@ namespace Collibri.Tests.Models.Posts
             Add(1, new List<Post>());
         }
     }
-<<<<<<< HEAD
     
     public class DeletePostByIdTestData : TheoryData<Guid, Post?, List<Post>>
     {
@@ -42,17 +33,6 @@ namespace Collibri.Tests.Models.Posts
             //Correct input
             Add(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"),
                 new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "title1", 1, 0, 0, 1,  new DateTime(), new DateTime()),
-=======
-
-    public class UpdatePostByIdTestData : TheoryData<Guid, Post, Post?, List<Post>>
-    {
-        public UpdatePostByIdTestData()
-        {
-            //Correct input
-            Add(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"),
-                new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "new title", 1, 2, 1, 1,  new DateTime(), new DateTime()),
-                new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "new title", 1, 2, 1, 1,  new DateTime(), new DateTime()),
->>>>>>> master
                 new List<Post>
                 {
                     new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "title1", 1, 0, 0, 1,  new DateTime(), new DateTime()),
@@ -60,7 +40,6 @@ namespace Collibri.Tests.Models.Posts
                     new Post(Guid.NewGuid(), "user3", "title3", 2, 0, 0, 1,  new DateTime(), new DateTime())
                 }
             );
-<<<<<<< HEAD
             //Failing input
             Add(new Guid("a4e38bc3-2dc0-4c61-a4c9-5ab59f8a5f57"),
                 null,
@@ -69,23 +48,6 @@ namespace Collibri.Tests.Models.Posts
                     new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "title1", 1, 0, 0, 1,  new DateTime(), new DateTime()),
                 }
             );
-=======
-            //Failing inputs
-            Add(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"),
-                new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "new title", 1, 2, 1, 1,  new DateTime(), new DateTime()),
-                null,
-                new List<Post>
-                {
-                    new Post(new Guid("222222a3-2297-48cf-9d4d-2222224a2262"), "user1", "title1", 1, 0, 0, 1, new DateTime(), new DateTime()),
-                    new Post(new Guid("222222a3-2297-48cf-9d4d-2222224a2261"), "user2", "title2", 1, 0, 0, 1, new DateTime(), new DateTime())
-                }
-            );
-            Add(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"),
-                new Post(new Guid("2b8b88a3-cd97-48cf-9d4d-ef8db4ac4a61"), "user1", "new title", 1, 2, 1, 1,  new DateTime(), new DateTime()),
-                null,
-                new List<Post>()
-            );
->>>>>>> master
         }
     }
 }
