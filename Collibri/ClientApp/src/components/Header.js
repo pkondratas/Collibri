e86fb6c89ca39e5baaf332435f1413d76282587e
migle-kirilovaite <migle.kirilovaite@gmail.com>
2023-10-08 23:06:21 +0300

    Replaced button text with icons

--- Collibri/ClientApp/src/components/Header.js
+++ Collibri/ClientApp/src/components/Header.js
@@ -4,12 +4,16 @@ const Header = () => {
     return (
         <header className="d-flex justify-content-between align-items-center mb-3" style={{ marginTop: '8px' }}>
             <div className="d-flex align-items-center">
-                <button className="btn btn-secondary" style={{ width: '90px', marginRight: '16px' }}>Go Back</button>
+                <button className="btn" style={{ width: '50px', marginRight: '16px' }}>
+                    <i className="fas fa-arrow-left"></i> {/* Font Awesome back arrow icon */}
+                </button>
                 <h2 className="mb-0">Collibri</h2>
             </div>
             <div className="d-flex align-items-center">
                 <input type="text" className="form-control mr-2" style={{ marginRight: '8px' }} placeholder="Enter text" />
-                <button className="btn btn-primary mr-2">Submit</button>
+                <button className="btn">
+                    <i className="fas fa-search"></i> {/* Font Awesome search icon */}
+                </button>
             </div>
         </header>
     );
