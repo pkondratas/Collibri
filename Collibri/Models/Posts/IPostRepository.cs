85a50860b43e586d3f1ad5c065c29f25947ab9d1
dovix58 <119929439+dovix58@users.noreply.github.com>
2023-10-12 19:30:21 +0300

    updateinu savo brancha sena, bo pats nesugeba pasiupdeitint

--- Collibri/Models/Posts/IPostRepository.cs
+++ Collibri/Models/Posts/IPostRepository.cs
@@ -5,6 +5,9 @@ namespace Collibri.Models.Posts
         public Post CreatePost(Post post);
 
         public IEnumerable<Post> GetAllPosts(int sectionId);
+
+        public Post? UpdatePostById(Guid postId, Post post);
+
+        public Post? DeletePostById(Guid postId);
     }
 }
-
