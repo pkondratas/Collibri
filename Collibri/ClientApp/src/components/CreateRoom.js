e802c17f55a9ea946578f41fb072e73dd7de3ef7
Jokubas Viliusis <viliusisj@gmail.com>
2023-10-12 16:53:38 +0300

    Paspaudus ant 'Create Room' mygtuko, dabar iššoka langelis (modal)

--- Collibri/ClientApp/src/components/CreateRoom.js
+++ Collibri/ClientApp/src/components/CreateRoom.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoom } from "./RoomAPI";
-import './LandingPage.css';
+import Modal from "react-modal";
+import "./LandingPageModal.css";
+
+const modalStyle = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
 
 export const CreateRoom = () => {
-    const handleCreateRoom = () => {
-        const inputRoomName = prompt('Enter Room Name:');
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [name, setName] = useState('');
+    function openModal() {
+        setIsOpen(true);
+    }
 
-        if (inputRoomName === null) {
+    function closeModal() {
+        setIsOpen(false);
+    }
+    
+    function handleCreateRoom() {
+        if(name === null || name === '') {
             return;
         }
-
-        createRoom(inputRoomName);
-    };
+        
+        closeModal();
+        createRoom(name);
+        setName(null);
+    }
 
     return (
         <div>
-            <button className={"btn buttons"} onClick={handleCreateRoom}>Create Room</button>
+            <button className={"btn buttons"} onClick={openModal}>Create Room</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={modalStyle}>
+                <h1>Create room</h1>
+                <label className={"message"}>Enter room name:</label>
+                <input 
+                    value={name} 
+                    onChange={e => setName(e.target.value)} />
+                <hr />
+                <button onClick={handleCreateRoom}>Confirm</button>
+                <button onClick={closeModal}>Cancel</button>
+                <div></div>
+            </Modal>
         </div>
     );
 }
