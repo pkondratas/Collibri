409f53ea4432aed2519c391921705dffea19d645
migle-kirilovaite <migle.kirilovaite@gmail.com>
2023-10-22 14:20:06 +0300

    Iškelti stiliai

--- Collibri/ClientApp/src/components/LoginPage.js
+++ Collibri/ClientApp/src/components/LoginPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { TextField, Button, Typography, Container, Paper, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Tooltip } from '@mui/material';
+import { LoginPageStyles } from '../styles/LoginPageStyles.js';
 
 const LoginPage = () => {
     return (
-        <Box style={{ display: 'grid', placeItems: 'center', height: '100vh', backgroundColor: '#ababab' }}>
-            <Box style={{ position: 'absolute', top: 0, left: 0, padding: '20px', zIndex: 1 }}>
-                <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold', color: '#000000' }}>
+        <Box style={LoginPageStyles.container}>
+            <Box style={LoginPageStyles.header}>
+                <Typography variant="h4" gutterBottom style={{ ...LoginPageStyles.typography, color: '#000000' }}>
                     Collibri
                 </Typography>
             </Box>
-            <Container maxWidth="xs" style={{ padding: '20px', borderRadius: '10px', textAlign: 'center', position: 'relative', zIndex: 0}}>
-                <Paper elevation={0} style={{ padding: 20, marginBottom: 50, borderRadius: 12, width: '100%', backgroundColor: '#d3d3d3'}}>
-                    <Typography variant="h5" gutterBottom style={{ fontWeight: 'bold', marginBottom: 20 }}>
+            <Container maxWidth="xs" style={LoginPageStyles.formContainer}>
+                <Paper elevation={0} style={LoginPageStyles.paper}>
+                    <Typography variant="h5" gutterBottom style={LoginPageStyles.typography}>
                         Login
                     </Typography>
                     <Box component="form" noValidate sx={{ mt: 1 }}>
@@ -24,7 +26,7 @@ const LoginPage = () => {
                             label="Username"
                             name="username"
                             autoComplete="username"
-                            sx={{ backgroundColor: '#ffffff', borderRadius: '8px' }}
+                            sx={LoginPageStyles.input}
                         />
                         <TextField
                             margin="normal"
@@ -35,16 +37,20 @@ const LoginPage = () => {
                             type="password"
                             id="password"
                             autoComplete="current-password"
-                            sx={{ backgroundColor: '#ffffff', borderRadius: '8px' }}
+                            sx={LoginPageStyles.input}
                         />
-                        <Typography variant="body2" style={{ marginBottom: 20 }}>
-                            <Link to="/forgot-password" style={{ textDecoration: 'none', color: '#757575', fontWeight: 'bold' }}>
-                                Forgot Password?
-                            </Link>
+                        <Typography variant="body2" style={LoginPageStyles.link}>
+                            <Tooltip title="Functionality not implemented" arrow>
+                                <Link to="/forgot-password" style={LoginPageStyles.link}>
+                                    Forgot Password?
+                                </Link>
+                            </Tooltip>
                         </Typography>
-                        <Button type="submit" fullWidth variant="contained" style={{ backgroundColor: '#757575', color: '#FFFFFF' }}>
-                            Login
-                        </Button>
+                        <Tooltip title="Functionality not implemented" arrow>
+                            <Button type="submit" fullWidth variant="contained" style={LoginPageStyles.button}>
+                                Login
+                            </Button>
+                        </Tooltip>
                     </Box>
                 </Paper>
             </Container>
@@ -53,4 +59,5 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
 
