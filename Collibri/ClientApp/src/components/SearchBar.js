1c1bba5d4436b3d47334abd87dfc0724b6565748
migle-kirilovaite <migle.kirilovaite@gmail.com>
2023-11-17 22:53:52 +0200

    Padariau, kad post modalas atsidarytu paspaudus pavadinima search bare

--- Collibri/ClientApp/src/components/SearchBar.js
+++ Collibri/ClientApp/src/components/SearchBar.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import {useEffect} from "react";
-import {fetchPosts} from "../api/PostAPI";
-import {createFilterOptions} from "@mui/material";
+import { useState, useEffect } from "react";
+import { fetchPosts } from "../api/PostAPI";
+import { createFilterOptions } from "@mui/material";
+import PostModal from "./PostModal";
 
 export default function SearchBar(props) {
+    const [selectedPost, setSelectedPost] = useState(null);
 
     const options = props.posts.map(post => ({
         label: post.title,
@@ -21,18 +23,33 @@ export default function SearchBar(props) {
         fetchPosts(props.sectionId, props.setPosts);
     }, [props.sectionId]);
 
-
+    const handlePostSelection = (event, value) => {
+        if (value) {
+            const selectedPost = props.posts.find(post => post.id === value.value);
+            setSelectedPost(selectedPost);
+        }
+    };
 
     return (
-        <Autocomplete
-            filterOptions={filterOptions}
-            disablePortal
-            id="combo-box-demo"
-            options={options}
-            getOptionLabel={(option) => option.label}
-            sx={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Enter post title" variant="filled" />}
-            noOptionsText={"There are no posts"}
-        />
+        <>
+            <Autocomplete
+                filterOptions={filterOptions}
+                disablePortal
+                id="combo-box-demo"
+                options={options}
+                getOptionLabel={(option) => option.label}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Enter post title" variant="filled" />}
+                noOptionsText={"There are no posts"}
+                onChange={handlePostSelection} 
+            />
+            {selectedPost && (
+                <PostModal
+                    {...selectedPost} 
+                    postModal={true}
+                    setPostModal={setSelectedPost}
+                />
+            )}
+        </>
     );
 }
