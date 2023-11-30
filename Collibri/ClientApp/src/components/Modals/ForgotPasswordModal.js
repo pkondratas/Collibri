c4d3b21a5f8aa6ce4605c48e94d8ca9ee56b129c
migle-kirilovaite <migle.kirilovaite@gmail.com>
2023-12-01 00:17:35 +0200

    Added loading indicator, success, failure screens, email regex check

--- Collibri/ClientApp/src/components/Modals/ForgotPasswordModal.js
+++ Collibri/ClientApp/src/components/Modals/ForgotPasswordModal.js
@@ -1,66 +1,125 @@
-import React, { useState } from 'react';
-import { Modal, TextField, Button, Box, Typography, Tooltip, IconButton } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Modal, TextField, Button, Box, Typography, IconButton, CircularProgress } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
-import modalStyles from "../../styles/ForgotPasswordModalStyles";
-import {sendEmail} from "../../api/ResetPasswordAPI"; 
-
+import CheckIcon from '@mui/icons-material/Check';
+import ErrorIcon from '@mui/icons-material/Error';
+import modalStyles from '../../styles/ForgotPasswordModalStyles';
+import { sendEmail } from '../../api/ResetPasswordAPI';
 
 const ForgotPasswordModal = ({ open, onClose }) => {
     const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(null);
+    const [emailValid, setEmailValid] = useState(true);
+
+    useEffect(() => {
+        if (open) {
+            setEmail('');
+            setLoading(false);
+            setSuccess(null);
+            setEmailValid(true);
+        }
+    }, [open]);
+
+    const isEmailValid = (email) => {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailRegex.test(email);
+    };
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
+        setEmailValid(true); // Reset the validation when the email changes
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
+        if (!email.trim() || !isEmailValid(email)) {
+            setEmailValid(false);
+            return;
+        }
+
         try {
-            await sendEmail({ "Email" : email });
-            onClose();
+            setLoading(true);
+            await sendEmail({ Email: email });
+            setSuccess(true);
         } catch (error) {
             console.error('Password reset failed:', error);
+            setSuccess(false);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <Modal open={open} onClose={onClose} centered>
-            <Box sx={modalStyles.container} position="relative">
-                <Box sx={modalStyles.closeButtonContainer}>
-                    <IconButton
-                        edge="end"
-                        color="inherit"
-                        onClick={onClose}
-                        aria-label="close"
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                </Box>
-                <Typography variant="h6" gutterBottom>
-                    Forgot Password
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        fullWidth
-                        margin="normal"
-                        label="Email"
-                        variant="outlined"
-                        value={email}
-                        onChange={handleEmailChange}
-                        sx={modalStyles.inputField}
-                    />
-
-                    <Tooltip title="Functionality not implemented" arrow>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={modalStyles.resetButton}
-                        >
-                            Reset Password
-                        </Button>
-                    </Tooltip>
-                </form>
+            <Box sx={modalStyles.container2}>
+                {!loading && !success && (
+                    <Box sx={modalStyles.closeButtonContainer}>
+                        <IconButton edge="end" color="inherit" onClick={onClose} aria-label="close">
+                            <CloseIcon />
+                        </IconButton>
+                    </Box>
+                )}
+                {success !== null ? (
+                    <Box sx={modalStyles.processing}>
+                        {success ? (
+                            <>
+                                <Typography>Email sent successfully!</Typography>
+                                <IconButton sx={modalStyles.checkIcon} onClick={onClose}>
+                                    <CheckIcon />
+                                </IconButton>
+                            </>
+                        ) : (
+                            <>
+                                <Typography>Error sending email</Typography>
+                                <IconButton sx={modalStyles.errorIcon} onClick={onClose}>
+                                    <ErrorIcon />
+                                </IconButton>
+                            </>
+                        )}
+                    </Box>
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        {loading ? (
+                            <Box sx={modalStyles.processing}>
+                                <Typography>Sending email...</Typography>
+                                <CircularProgress color="inherit" />
+                            </Box>
+                        ) : (
+                            <>
+                                <Typography variant="h6" gutterBottom>
+                                    Forgot Password
+                                </Typography>
+                                <TextField
+                                    fullWidth
+                                    margin="normal"
+                                    label="Email"
+                                    variant="outlined"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    sx={modalStyles.inputField}
+                                />
+                                <Typography
+                                    sx={{
+                                        ...modalStyles.errorHelperText,
+                                        visibility: emailValid ? 'hidden' : 'visible', 
+                                    }}
+                                >
+                                    Incorrect format (ex: something@smth.com)
+                                </Typography>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={modalStyles.resetButton}
+                                >
+                                    Reset Password
+                                </Button>
+                            </>
+                        )}
+                    </form>
+                )}
             </Box>
         </Modal>
     );
