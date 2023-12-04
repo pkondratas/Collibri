c72cb7b625b1d0e2d73c72e4980c974affff1900
dovix58 <119929439+dovix58@users.noreply.github.com>
2023-12-04 19:50:51 +0200

    padariau bazini toki information page

--- Collibri/ClientApp/src/styles/TabStyles.js
+++ Collibri/ClientApp/src/styles/TabStyles.js
@@ -10,6 +10,23 @@ const tabStyles = {
     question: {
         paddingBottom: '1.2rem',
         fontWeight: 'bold'
+    },
+    answer:{
+        paddingBottom: '0.6rem',
+    },
+    reviews:{
+        paddingLeft: '30%',
+        paddingRight: '30%',
+    },
+    comments:{
+         paddingTop: '0.6rem', 
+         paddingLeft: '1rem',
+         fontSize : '2rem',
+        fontFamily: 'Merriweather'
+    },
+    commentBox:{
+        display:'flex', 
+        paddingBottom: '1.5rem'
     }
 };
 export default tabStyles;
