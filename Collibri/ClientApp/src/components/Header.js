bb7d3588414d0f5d3ff27497359479ea82015f6c
migle-kirilovaite <migle.kirilovaite@gmail.com>
2023-12-09 00:45:12 +0200

    Moved room settings to header

--- Collibri/ClientApp/src/components/Header.js
+++ Collibri/ClientApp/src/components/Header.js
@@ -1,32 +1,30 @@
+// Header.js
 import React from 'react';
-import { AppBar, Toolbar, Box, IconButton, Typography, OutlinedInput, InputAdornment, Tooltip } from '@mui/material';
+import { AppBar, Toolbar, Box, IconButton, Typography, Tooltip } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import SearchIcon from '@mui/icons-material/Search';
-import { HeaderStyles } from '../styles/HeaderStyles'; 
-import {useNavigate} from "react-router-dom";
-import SearchBar from "./SearchBar";
-import {AboutUsButton} from "./Buttons/AboutUsButton";
+import { HeaderStyles } from '../styles/HeaderStyles';
+import { useNavigate } from 'react-router-dom';
+import { AboutUsButton } from './Buttons/AboutUsButton';
 
-const Header = () => {
-    
+const Header = ({ roomSettings }) => {
     const navigate = useNavigate();
-    
+
     return (
         <AppBar position="static" style={HeaderStyles.appBar}>
             <Toolbar style={HeaderStyles.toolbar}>
                 <Box style={HeaderStyles.box}>
                     <Tooltip title="Go back to room selection">
-                        <IconButton color="inherit" aria-label="Back" onClick={() => navigate("/home")}>
+                        <IconButton color="inherit" aria-label="Back" onClick={() => navigate('/home')}>
                             <ArrowBackIcon style={HeaderStyles.backButton} />
                         </IconButton>
                     </Tooltip>
-                    <Typography variant="h4" style={HeaderStyles.title}>Collibri</Typography>
                 </Box>
                 <Box>
-                    <AboutUsButton />
+                    {roomSettings} {/* Render RoomSettings component here */}
                 </Box>
             </Toolbar>
         </AppBar>
     );
-}
+};
+
 export default Header;
