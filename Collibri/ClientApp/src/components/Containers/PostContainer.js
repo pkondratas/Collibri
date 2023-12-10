f473bd1ad14f09b6a2e4e066cba009396e9e28c6
Jokūbas Viliušis <viliusisj@gmail.com>
2023-12-10 22:53:16 +0200

    PostContaineris, kai nera postu arba kai nepaselectintas

--- Collibri/ClientApp/src/components/Containers/PostContainer.js
+++ Collibri/ClientApp/src/components/Containers/PostContainer.js
@@ -13,9 +13,12 @@ const PostContainer = (props) => {
   if(props.sectionId === 0) {
       return (
           <>
-              <Box sx={containerStyle}>
-                  <Typography>
-                      No section selected.
+              <Box sx={containerStyle.container}>
+                  <Typography variant="h4" sx={containerStyle.message}>
+                      No section selected
+                  </Typography>
+                  <Typography sx={containerStyle.description}>
+                      Click on a section to view posts!
                   </Typography>
               </Box>
           </>
@@ -24,9 +27,12 @@ const PostContainer = (props) => {
   else if (props.posts.length === 0) {
       return (
           <>
-              <Box sx={containerStyle}>
-                  <Typography>
-                      No posts created.
+              <Box sx={containerStyle.container}>
+                  <Typography variant="h4" sx={containerStyle.message}>
+                      No posts created
+                  </Typography>
+                  <Typography sx={containerStyle.description}>
+                      Add a post to this section!
                   </Typography>
               </Box>
           </>
@@ -35,7 +41,7 @@ const PostContainer = (props) => {
   else {
       return (
           <>
-              <Box sx={containerStyle}>
+              <Box sx={containerStyle.container}>
                   <List>
                       {props.posts.map(item => (
                           <ListItem key={item.id}>
--- Collibri/ClientApp/src/styles/PostContainerStyle.js
+++ Collibri/ClientApp/src/styles/PostContainerStyle.js
@@ -1,7 +1,20 @@
 export const containerStyle = {
-  backgroundColor: '#DEFEF5',
-  minHeight: "30rem", 
-  maxHeight: "30rem", 
-  overflowY: "auto", 
-  maxWidth: "43rem" 
+  container: {
+    backgroundColor: '#DEFEF5',
+    minHeight: "30rem",
+    maxHeight: "30rem",
+    overflowY: "auto",
+    maxWidth: "43rem"
+  },
+  message: {
+    fontFamily: 'Segoe UI semibold',
+    margin: '5%',
+    marginBottom: '2%',
+    color: '#316C44'
+  },
+  description: {
+    fontFamily: 'Segoe UI semibold',
+    marginLeft: '5%',
+    color: '#80CB9E'
+  }
 }
