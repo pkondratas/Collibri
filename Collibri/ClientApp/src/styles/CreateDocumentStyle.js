43eef667057d43670a8b321d7a8e3512c8251100
Jokūbas Viliušis <viliusisj@gmail.com>
2023-12-11 14:06:12 +0200

    CreateNoteModal

---
+++ Collibri/ClientApp/src/styles/CreateDocumentStyle.js
@@ -0,0 +1,44 @@
+export const CreateDocumentStyle = {
+    modalWindow: {
+        backgroundColor: '#eee',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        border: '2px solid #000',
+        p: 2,
+        bgcolor: '#DEFEF5',
+        borderRadius: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    textFieldBox: {
+        ml: 'auto',
+        mr: 'auto',
+        display: 'flex',
+    },
+
+    nameTextField: {
+        width: 750,
+        mb: 2,
+    },
+
+    warningNote: {
+        ml: 'auto',
+        mr: 'auto',
+        mt: 2,
+        mb: 2,
+    },
+
+    descriptionTextField: {
+        width: 750
+    },
+
+    buttonBox: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+}
